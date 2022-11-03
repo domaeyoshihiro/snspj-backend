8da1b95c6bee93c32d8a57253fef9a533b3d1dba
domaeyoshihiro <e8jbb@yahoo.co.jp>
2022-11-03 18:32:38 +0900

    snspj-back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $items = User::all();
-        return response()->json([
-        'data' => $items
-        ], 200);
+    public function __construct() {
+        $this->middleware('firebase');
     }
+
+    public function __invoke(Request $request): JsonResponse
+    {
+        $firebaseId = $request->firebaseId;
+        $email = $request->email;
+        var_dump($firebaseId);
+        var_dump($email);
+
+        $response = [
+            'firebase_id' => $firebaseId,
+            'email' => $email,
+        ];
+
+        var_dump($response);
+
+        return response()->json($response, Response::HTTP_CREATED);
+    }
+
+    public function store(Request $request) {
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        return response()->json(['message' => 'Successfully user create']);
+    }
+
 }
+
--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\CommentController;
 
-Route::apiResource('/v1/user', UserController::class)->only(['index']);
+Route::apiResource('/v1/user', UserController::class)->only(['store']);
 Route::apiResource('/v1/post', PostController::class)->only(['index', 'store', 'show', 'destroy']);
 Route::apiResource('/v1/like', LikeController::class)->only(['store', 'destroy']);
 Route::apiResource('/v1/comment', CommentController::class)->only(['store']);
