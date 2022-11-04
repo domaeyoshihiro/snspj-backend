6d45bdfaf7ad37d4771588fc0624d71572bf5234
domaeyoshihiro <e8jbb@yahoo.co.jp>
2022-11-04 17:41:03 +0900

    snspj-back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,37 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function __construct() {
-        $this->middleware('firebase');
-    }
-
-    public function __invoke(Request $request): JsonResponse
-    {
-        $firebaseId = $request->firebaseId;
-        $email = $request->email;
-        var_dump($firebaseId);
-        var_dump($email);
-
-        $response = [
-            'firebase_id' => $firebaseId,
-            'email' => $email,
-        ];
-
-        var_dump($response);
-
-        return response()->json($response, Response::HTTP_CREATED);
-    }
-
     public function store(Request $request) {
         User::create([
             "name" => $request->name,
             "email" => $request->email,
-            "password" => Hash::make($request->password)
+            "firebaseid" => $request->firebaseid
         ]);
 
         return response()->json(['message' => 'Successfully user create']);
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,6 +11,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('firebaseid')->unique();
+            $table->string('email');
             $table->timestamps();
         });
     }
