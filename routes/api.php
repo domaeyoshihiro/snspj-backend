92cc40a9193bbda311b6ed87135816396b77d8fb
domaeyoshihiro <e8jbb@yahoo.co.jp>
2022-11-12 20:36:33 +0900

    snspj-back

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,12 @@ use App\Http\Controllers\LikeController;
 use App\Http\Controllers\CommentController;
 
 Route::apiResource('/user', UserController::class)->only(['store']);
-Route::apiResource('/post', PostController::class)->only(['index', 'store', 'show', 'destroy']);
+Route::apiResource('/post', PostController::class)->only(['index', 'store', 'show']);
+Route::post('/post/destroy/{id}', [PostController::class, 'destroy']);
+
+
+
 Route::apiResource('/like', LikeController::class)->only(['store', 'destroy']);
 Route::post('/like/count', 'LikeController@count');
 Route::apiResource('/comment', CommentController::class)->only(['store']);
+
