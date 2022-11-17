82758ee28d8264d640b504daed81fb43257f05c3
domaeyoshihiro <e8jbb@yahoo.co.jp>
2022-11-17 13:14:58 +0900

    sns-back

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     public function index(Request $request)
     {
         $user = User::where("firebaseid", $request -> firebaseid )->first();
-        $items = ['users' => $users];
+        $items = ['user' => $user];
         return response()->json([
         'data' => $items
         ], 200);
